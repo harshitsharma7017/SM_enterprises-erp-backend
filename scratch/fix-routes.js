109f6ea9572a1f9423149e731a1f72e5c799715b
Harshit Sharma <harshit.sharma8532@gmail.com>
2026-09-22 15:45:43 +0530

    chore: remove obsolete backend scratch scripts

--- scratch/fix-routes.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-const file = 'src/modules/export-document/export-document.routes.js';
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace(/\/:document\/packing-list\/\{\:variant\}\?/g, '/:document/packing-list/:variant');
-fs.writeFileSync(file, content);
